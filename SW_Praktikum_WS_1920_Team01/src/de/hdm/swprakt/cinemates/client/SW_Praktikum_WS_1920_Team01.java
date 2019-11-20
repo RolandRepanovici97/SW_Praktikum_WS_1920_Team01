1a28733ce824b7d232ca410e00badb4766b7bb4e
Alina Hafner <ah200@hdm-stuttgart.de>
2019-11-20 11:47:06 +0100

    Projektstruktur anlegen

---
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/SW_Praktikum_WS_1920_Team01.java
@@ -0,0 +1,16 @@
+package de.hdm.swprakt.cinemates.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class SW_Praktikum_WS_1920_Team01 implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}

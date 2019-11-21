c504a156985954b31f3ec557c86c1a263432f24b
Alina Hafner <ah200@hdm-stuttgart.de>
2019-11-21 21:40:08 +0100

    VotumMapper Keine Sorge das dient nur zur Vorbereitung

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/UmfrageeintragMapper.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/UmfrageeintragMapper.java
@@ -6,9 +6,9 @@ package de.hdm.swprakt.cinemates.server.db;
 /**
  * Diese Mapperklasse bildet <code>Umfrageeintrag</code> Objekte auf eine
  * relationale Datenbank ab. 
- * Sie beinhalte Methoden, um Umfrageeinntäge anzeigen, anlegen, editieren und löschen zu können.
- * Es können hier sowohl Objekte der Klasse <code> Umfrageeintrag </code> in Datenbankstrukturen,
- * als auch Datenbankstrukturen in Objekte der Klasse <code> Umfrageeintrag </code>  umgewandelt werden.
+ * Sie beinhalt Methoden, um Umfrageeinträge anzeigen, anlegen, editieren und löschen zu können.
+ * Es können hier sowohl Objekte der Klasse <code>Umfrageeintrag</code> in Datenbankstrukturen,
+ * als auch Datenbankstrukturen in Objekte der Klasse <code>Umfrageeintrag</code>  umgewandelt werden.
  * 
  * @author alina
  * @version 1.0
@@ -16,9 +16,9 @@ package de.hdm.swprakt.cinemates.server.db;
  */
 public class UmfrageeintragMapper {
 
-	/**Die Klasse <code> UmfrageeintragMapper </code> wird wie jede andere Mapperklasse 
+	/**Die Klasse <code>UmfrageeintragMapper</code> wird wie jede andere Mapperklasse 
 	 * nur ein einziges mal instantiiert. Die Variable umfrageeintragMapper speichert diese Instanz.
-	 * Wir stellen die einmalige Initalisierung durch den Bezeichner <code> static </code> sicher. 
+	 * Wir stellen die einmalige Initalisierung durch den Bezeichner <code>static</code> sicher. 
 	 * 
 	 */
 	private static UmfrageeintragMapper umfrageeintragMapper = null;
@@ -26,18 +26,18 @@ public class UmfrageeintragMapper {
 
 	/** 
 	 * Ein geschützter Konstruktor verhindert die Möglichkeit, mit <code>new</code>
-	 * neue Instanzen der Klasse <code> UmfrageeintragMapper </code> zu erzeugen.
+	 * neue Instanzen der Klasse <code>UmfrageeintragMapper</code> zu erzeugen.
 	 * 
 	 */
 	protected UmfrageeintragMapper() {
 
 	}
-	/** Mithilfe dieser statischen Methode kann die einzige Instanz der Klasse <code> UmfrageeintragMapper </code>
+	/** Mithilfe dieser statischen Methode kann die einzige Instanz der Klasse <code>UmfrageeintragMapper</code>
 	 * erzeugt werden. Die Methode beinhaltet die Prüfung, ob bereits ein UmfrageeintragMapper-Objekt erzeugt ist,
 	 * dadurch wird sichergestellt, dass immer nur ein Objekt dieser Klasse erzeugt ist.
-	 * Ist bereits ein <code> UmfrageeintragMapper </code> - Objekt erzeugt worden, so geben wir die Referenz auf dieses zurück.
+	 * Ist bereits ein <code>UmfrageeintragMapper</code> - Objekt erzeugt worden, so geben wir die Referenz auf dieses zurück.
 	 * 
-	 * @return das einzige <code> umfrageeintragMapper </code> - Objekt
+	 * @return das einzige <code>umfrageeintragMapper</code> - Objekt
 	 */
 
 	public static UmfrageeintragMapper umfrageeintragMapper() {

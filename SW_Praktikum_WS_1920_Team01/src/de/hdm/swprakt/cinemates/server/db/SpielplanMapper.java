ae69cadbf6b2178940f6b343679c56d061ef11a1
ÖmerDegirmenci <oemer.degirmenci@hotmail.de>
2019-11-21 21:47:58 +0100

    Erstellung der Klasse Spielplan

---
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/SpielplanMapper.java
@@ -0,0 +1,29 @@
+package de.hdm.swprakt.cinemates.server.db;
+
+
+
+
+
+/**
+ * Diese Mapperklasse bildet <code>Spielplan</code> Objekte auf eine
+ * relationale Datenbank ab. 
+ * Sie beinhalte Methoden, um Spielpläne anzeigen, anlegen, editieren und löschen zu können.
+ * Es können hier sowohl Objekte der Klasse <code> Spielplan </code> in Datenbankstrukturen,
+ * als auch Datenbankstrukturen in Objekte der Klasse <code> Spielplan </code>  umgewandelt werden.
+ * 
+ * @author Ömer Degirmenci
+ * @version 1.0
+ *
+ */
+
+public class SpielplanMapper {
+	
+	
+	
+	/**
+	 * 
+	 */
+	
+	private static SpielplanMapper spielplanmapper = null;
+
+}

ea7c3f68bd664b3621c25bec4035fb17bb1ef4a9
ÖmerDegirmenci <oemer.degirmenci@hotmail.de>
2019-11-21 21:47:41 +0100

    Erstelleung der Klasse SpielzeitMapper

---
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/SpielzeitMapper.java
@@ -0,0 +1,25 @@
+package de.hdm.swprakt.cinemates.server.db;
+
+
+
+/**
+ * Diese Mapperklasse bildet <code>Spielzeit</code> Objekte auf eine
+ * relationale Datenbank ab. 
+ * Sie beinhalte Methoden, um Filme anzeigen, anlegen, editieren und löschen zu können.
+ * Es können hier sowohl Objekte der Klasse <code> Spielzeit </code> in Datenbankstrukturen,
+ * als auch Datenbankstrukturen in Objekte der Klasse <code> Spielzeit</code>  umgewandelt werden.
+ * 
+ * @author Ömer Degirmenci 
+ * @version 1.0
+ *
+ */
+public class SpielzeitMapper {
+	
+	/**
+	 * 
+	 */
+	
+	private static SpielzeitMapper spielzeitmapper = null;
+
+
+}

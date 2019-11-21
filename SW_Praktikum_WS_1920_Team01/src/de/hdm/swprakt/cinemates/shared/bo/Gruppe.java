1b9053b07102e645012ae0c7d1e8724d2220906c
Jon Gehrung <jg138@hdm-stuttgart.de>
2019-11-21 08:26:47 +0100

    Business Objekte Nutzer und Gruppe

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/shared/bo/Gruppe.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/shared/bo/Gruppe.java
@@ -1,5 +1,45 @@
 package de.hdm.swprakt.cinemates.shared.bo;
 
-public class Gruppe {
+import java.util.Vector;
 
+public class Gruppe extends BusinessObject{
+	
+	private String gruppenname;
+	private int ersteller_id;
+	private Vector<Integer> gruppenmitglieder;
+	
+	public Gruppe() {
+		super();
+	}
+
+	public String getGruppenname() {
+		return gruppenname;
+	}
+
+	public void setGruppenname(String gruppenname) {
+		this.gruppenname = gruppenname;
+	}
+
+	public int getErstellerId() {
+		return ersteller_id;
+	}
+
+	public void setErstellerId(int ersteller_id) {
+		this.ersteller_id = ersteller_id;
+	}
+	
+	public void addGrupenmitglied(int nutzerkonto_id) {
+		gruppenmitglieder.add(nutzerkonto_id);
+	}
+	
+	public Vector<Integer> getGruppenmitglieder(){
+		return gruppenmitglieder;
+	}
+	
+	public void setGruppenmitglieder(Vector<Integer> gruppenmitglieder) {
+		this.gruppenmitglieder = gruppenmitglieder;
+	}
+	
+	
+		
 }

3f13694abbbbe00f14ebd8a78b354c2d7d88ada9
Alina Hafner <ah200@hdm-stuttgart.de>
2019-11-24 18:36:05 +0100

    Verweis auf Mapper in jeweiliger Impl-Klasse.

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/KinoAdministrationImpl.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/KinoAdministrationImpl.java
@@ -5,6 +5,12 @@ package de.hdm.swprakt.cinemates.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.swprakt.cinemates.server.db.FilmMapper;
+import de.hdm.swprakt.cinemates.server.db.KinoMapper;
+import de.hdm.swprakt.cinemates.server.db.KinoketteMapper;
+import de.hdm.swprakt.cinemates.server.db.NutzerMapper;
+import de.hdm.swprakt.cinemates.server.db.SpielplanMapper;
+import de.hdm.swprakt.cinemates.server.db.SpielzeitMapper;
 import de.hdm.swprakt.cinemates.shared.KinoAdministration;
 
 /**
@@ -17,4 +23,14 @@ import de.hdm.swprakt.cinemates.shared.KinoAdministration;
  */
 public class KinoAdministrationImpl extends RemoteServiceServlet implements KinoAdministration{
 
+/** Der Kinobetreiber benötigt Zuriff auf die Daten rund um die Verwaltung eines Kinos.
+ *  Dieser Zugriff wird über die jeweiligen Mapper realisiert. 
+ * 
+ */
+private KinoMapper kinoMapper = null;
+private KinoketteMapper kinoketteMapper = null;
+private FilmMapper filmMapper = null;
+private SpielzeitMapper spielzeitMapper = null;
+private SpielplanMapper spielplanMapper = null;
+private NutzerMapper nutzerMapper = null;
 }

2240748938b36b590426f6e7783aeb86190a1cff
Alina Hafner <ah200@hdm-stuttgart.de>
2019-11-24 12:00:28 +0100

    Implementierung des Headers der Applikation.

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/SW_Praktikum_WS_1920_Team01.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/SW_Praktikum_WS_1920_Team01.java
@@ -2,6 +2,7 @@ package de.hdm.swprakt.cinemates.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
+
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
@@ -10,6 +11,7 @@ public class SW_Praktikum_WS_1920_Team01 implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
+		
 		
 	}
 	

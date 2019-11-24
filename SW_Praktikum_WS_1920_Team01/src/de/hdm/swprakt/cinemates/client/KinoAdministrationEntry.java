f6adf10cede4835b60f5a59601427492030fc170
Alina Hafner <ah200@hdm-stuttgart.de>
2019-11-24 12:14:05 +0100

    Entry-Point Klassen sinnvoll benannt.

---
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/KinoAdministrationEntry.java
@@ -0,0 +1,18 @@
+package de.hdm.swprakt.cinemates.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class KinoAdministrationEntry implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+}
--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/KinobesuchsplanungEntry.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/KinobesuchsplanungEntry.java
@@ -6,7 +6,7 @@ import com.google.gwt.core.client.EntryPoint;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class SW_Praktikum_WS_1920_Team01 implements EntryPoint {
+public class KinobesuchsplanungEntry implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {

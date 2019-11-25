adc97c5c56bd21bdffc9e958ff5136e3d507034b
Alina Hafner <ah200@hdm-stuttgart.de>
2019-11-25 22:55:09 +0100

    Weitere Ergänzungen in Mapperklassen

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/VotumMapper.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/VotumMapper.java
@@ -4,11 +4,13 @@
 package de.hdm.swprakt.cinemates.server.db;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
 
+import de.hdm.swprakt.cinemates.shared.bo.Umfrageeintrag;
 import de.hdm.swprakt.cinemates.shared.bo.Votum;
 
 /**
@@ -29,7 +31,7 @@ public class VotumMapper {
 	 * 
 	 */
 	private static VotumMapper votumMapper = null;
-	private static DateConverter tsm = new DateConverter();
+	private static DateConverter dc = new DateConverter();
 
 	/** 
 	 * Ein geschützter Konstruktor verhindert die Möglichkeit, mit <code>new</code>
@@ -99,6 +101,11 @@ public class VotumMapper {
 
 		return null;
 	}
+	/**
+	 * Suchen aller Objekte der Klasse <code> Votum </code>
+	 * @return Vector <Votum>, welcher alle Votum-Objekte beinhaltet
+	 */
+
 	public Vector <Votum> findAll() {
 		//Verbindung zur Datenbank aufbauen.
 		Connection con = DBConnection.connection();
@@ -116,7 +123,7 @@ public class VotumMapper {
 				// Es werden für jedes Votum-Objekt die nötigen Attribute gesetzt
 				Votum votum = new Votum();
 				votum.setID(rs.getInt("votum_id"));
-				votum.setErstellungszeitpunkt(tsm.convertTimestampToDate(rs.getTimestamp("Erstellungszeitpunkt")));
+				votum.setErstellungszeitpunkt(dc.convertTimestampToDate(rs.getTimestamp("Erstellungszeitpunkt")));
 				votum.setOwnerID(rs.getInt("bo_id"));
 				votum.setIstMöglicherTermin(rs.getBoolean("istMöglicherTermin"));
 				votum.setUmfrageeintragID(rs.getInt("umfrageeintrag_id"));
@@ -134,4 +141,167 @@ public class VotumMapper {
 
 		return null;
 	}
+
+	/**
+	 * Suchen aller Objekte der Klasse <code> Votum</code>, welche zu einem übergebenen Umfrageeintrag gehören.
+	 * @param Objekt der Klasse <Umfrageeintrag>
+	 * @return Vector <Votum>, welcher alle Votum-Objekte beinhaltet, welche dem übergebenen Umfrageeintrag gehören
+	 */
+
+
+	public Vector <Votum> findVotumByUmfrageeintrag(Umfrageeintrag umfrageeintrag) {
+		//Verbindung zur Datenbank aufbauen.
+		Connection con = DBConnection.connection();
+		// Neuen Vector instantiieren, in welchem unsere Votum-Objekte gespeichert werden
+		Vector <Votum> vectorvotum = new Vector <Votum>();
+		try {
+			// Leeres SQL-Statement (JDBC) anlegen
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM votum LEFT JOIN ownedbusinessobject ON nutzer.bo_id = ownedbusinessobject.bo_Id WHERE votum.umfrageeintrag_id="+
+					umfrageeintrag.getID()+ "ORDER BY umfrageeintrag_id");
+
+			if (rs.next()) {
+				// Es werden für jedes Votum-Objekt die nötigen Attribute gesetzt
+				Votum votum = new Votum();
+				votum.setID(rs.getInt("votum_id"));
+				votum.setErstellungszeitpunkt(d.convertTimestampToDate(rs.getTimestamp("Erstellungszeitpunkt")));
+				votum.setOwnerID(rs.getInt("bo_id"));
+				votum.setIstMöglicherTermin(rs.getBoolean("istMöglicherTermin"));
+				votum.setUmfrageeintragID(rs.getInt("umfrageeintrag_id"));
+				vectorvotum.add(votum);
+
+
+
+				return vectorvotum;
+
+			}
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * Einfügen eines Votum-Objekts in die Datenbank.
+	 * @return Ein Objekt der Klasse <Votum>
+	 */
+
+	public Votum insert (Votum votum) {
+
+		//Verbindung zur Datenbank aufbauen.
+
+		Connection con = DBConnection.connection();
+
+		try {
+			// Leeres SQL-Statement (JDBC) anlegen
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT MAX(votum_id) AS maxid FROM votum");
+
+			if (rs.next()) {
+				votum.setID(rs.getInt("maxid") + 1);
+			}	
+
+			PreparedStatement pstmt = con.prepareStatement("INSERT INTO votum(votum_id, erstellungszeitpunkt"
+					+ "owner_id, istMöglicherTermin,umfrageeintrag_id VALUES (?, ?, ?, ?,?) ");
+			pstmt.setInt(1,votum.getID());
+			pstmt.setTimestamp(2, dc.aktuellerTimestamp());
+			votum.setErstellungszeitpunkt(dc.convertTimestampToDate(dc.aktuellerTimestamp()));
+			pstmt.setInt(3, votum.getOwnerID());
+			pstmt.setBoolean(4, votum.getisIstMölicherTermin());
+			pstmt.setInt(5, votum.getUmfrageeintragID());
+			pstmt.executeUpdate();
+			return votum;
+
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Aktualisieren eines Votum-Objekts in der Datenbank. Achtung: Es können nicht alle Attribute aktualisiert werden.
+	 * Es wurde auf weniger sinnvolle Möglichkeiten verzichtet.
+	 * @return Ein (aktualisiertes) Objekt der Klasse <code>Votum</code>
+	 */
+
+	public Votum update (Votum votum) {
+
+		//Verbindung zur Datenbank aufbauen.
+
+		Connection con = DBConnection.connection();
+
+		try {
+
+			PreparedStatement pstmt = con.prepareStatement("UPDATE votum SET istMöglicherTermin =?");
+
+
+			pstmt.setBoolean(4, votum.getisIstMölicherTermin());
+
+			pstmt.executeUpdate();
+			return votum;
+
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/**
+	 * Löschen eines Votum-Objekts in der Datenbank.
+		 @param Objekt der Klasse <code>Votum</code>
+	 * 
+	 */
+
+	public void delete (Votum votum) {
+
+		//Verbindung zur Datenbank aufbauen.
+
+		Connection con = DBConnection.connection();
+
+		try {
+			// Leeres SQL-Statement (JDBC) anlegen
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("DELETE FROM votum WHERE votum_id=" + votum.getID());
+
+
+
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
+	/**
+	 * Löschen der Votum-Objekte, welche zu einem Umfrageeintrag gehören.
+	 * @param Objekt der Klasse <code>Umfrageeintrag</code>
+	 */
+
+	public void deleteVotumByUmfrage (Umfrageeintrag umfrageeintrag) {
+
+		//Verbindung zur Datenbank aufbauen.
+
+		Connection con = DBConnection.connection();
+
+		try {
+			// Leeres SQL-Statement (JDBC) anlegen
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("DELETE FROM votum WHERE umfrageeintrag_id=" + umfrageeintrag.getID());
+
+
+
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
+
 }

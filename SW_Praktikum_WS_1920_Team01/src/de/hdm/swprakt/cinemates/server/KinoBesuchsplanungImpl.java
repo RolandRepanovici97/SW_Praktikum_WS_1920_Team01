c0effcb6ffc120219e5c4d326eba61abb8b39594
Jon Gehrung <jg138@hdm-stuttgart.de>
2019-11-26 15:49:02 +0100

    Implementierung des LoginService mit vorläufigen Rumpfmethoden im KinoBesuchsplanung Service für das Einfügen eines neuen Nutzers

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/KinoBesuchsplanungImpl.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/KinoBesuchsplanungImpl.java
@@ -7,11 +7,16 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.hdm.swprakt.cinemates.server.db.GruppeMapper;
 import de.hdm.swprakt.cinemates.server.db.NutzerMapper;
+import de.hdm.swprakt.cinemates.server.db.OwnedBusinessObjectMapper;
 import de.hdm.swprakt.cinemates.server.db.UmfrageMapper;
 import de.hdm.swprakt.cinemates.server.db.UmfrageeintragMapper;
 import de.hdm.swprakt.cinemates.server.db.VotumMapper;
 import de.hdm.swprakt.cinemates.shared.KinoAdministration;
 import de.hdm.swprakt.cinemates.shared.KinoBesuchsplanung;
+import de.hdm.swprakt.cinemates.shared.bo.Nutzer;
+import de.hdm.thies.bankProjekt.server.db.AccountMapper;
+import de.hdm.thies.bankProjekt.server.db.CustomerMapper;
+import de.hdm.thies.bankProjekt.server.db.TransactionMapper;
 
 /**
  * Diese Klasse stellt die Implementierungsklasse des Interface {@link KinoBesuchsplanung} dar. 
@@ -38,5 +43,42 @@ private GruppeMapper gruppeMapper = null;
 private UmfrageMapper umfrageMapper = null;
 private UmfrageeintragMapper umfrageeintragMapper = null;
 private VotumMapper votumMapper = null;
+private OwnedBusinessObjectMapper ownedBusinessObjectMapper = null;
+
+
+public KinoBesuchsplanungImpl() throws IllegalArgumentException {
+    
+  }
+
+ 
+  @Override
+public void init() throws IllegalArgumentException {
+    
+    this.nutzerMapper = NutzerMapper.nutzerMapper();
+    this.gruppeMapper = GruppeMapper.gruppeMapper();
+    this.umfrageMapper = UmfrageMapper.umfrageMapper();
+    this.umfrageeintragMapper = UmfrageeintragMapper.umfrageeintragMapper();
+    this.votumMapper = VotumMapper.votumMapper();
+    this.ownedBusinessObjectMapper = OwnedBusinessObjectMapper.ownedBusinessObjectMapper();
+    
+    KinoAdministrationImpl kai = new KinoAdministrationImpl();
+    kai.init();
+    this.administration = kai;
+  }
+  
+@Override
+public Nutzer findNutzerByEmail(String email) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public Nutzer insertNutzer(Nutzer nutzer) {
+	
+	//Einfügen eines neuen OwnedBusinessObjects
+	//Einfügen des Nutzers in die Nutzerdatenbank mit zugehöriger OwnedBusinessObject Referenz
+	// TODO Auto-generated method stub
+	return null;
+}
 
 }
--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/shared/KinoBesuchsplanungAsync.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/shared/KinoBesuchsplanungAsync.java
@@ -3,6 +3,10 @@
  */
 package de.hdm.swprakt.cinemates.shared;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.hdm.swprakt.cinemates.shared.bo.Nutzer;
+
 /**
  * Das asynchrone Gegenstück des Interface {@link KinoBesuchsplanung}
  * @author alina 
@@ -11,4 +15,7 @@ package de.hdm.swprakt.cinemates.shared;
  */
 public interface KinoBesuchsplanungAsync {
 
+	public void findNutzerByEmail(String email, AsyncCallback<Nutzer> callback);
+	
+	public void insertNutzer(Nutzer nutzer, AsyncCallback<Nutzer> callback);
 }

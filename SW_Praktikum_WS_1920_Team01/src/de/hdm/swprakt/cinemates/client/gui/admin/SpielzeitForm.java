30e9b04b995442909171675be89f5d6bd2f09b07
ÖmerDegirmenci <oemer.degirmenci@hotmail.de>
2019-12-18 13:24:17 +0100

    SpielzeitForm angelegt

---
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/admin/SpielzeitForm.java
@@ -0,0 +1,11 @@
+package de.hdm.swprakt.cinemates.client.gui.admin;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TextBox;
+
+public class SpielzeitForm extends HorizontalPanel {
+	
+	
+
+}

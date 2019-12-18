c7415cea7a29e66f51e1a7097038e27a8b9a6371
ÖmerDegirmenci <oemer.degirmenci@hotmail.de>
2019-12-18 12:47:39 +0100

    Grid erstellt

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/admin/SpielplanForm.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/admin/SpielplanForm.java
@@ -1,29 +1,34 @@
 package de.hdm.swprakt.cinemates.client.gui.admin;
 
 import com.google.gwt.user.client.ui.Grid;
+
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
+
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class SpielplanForm extends HorizontalPanel{
 
-	
+public class SpielplanForm extends HorizontalPanel {
+
 	TextBox spielplanName = new TextBox();
 	ListBox filmAuswählen = new ListBox();
-	
+
+	FilmForm ffm = new FilmForm();
+
 	HorizontalPanel detailsPanel = new HorizontalPanel();
-	
+
 	public void onLoad() {
+
 		
+
 		super.onLoad();
-		
+
 		Grid spielplanGrid = new Grid(4, 4);
 		spielplanGrid.setWidget(0, 0, filmAuswählen);
-		
+
 		detailsPanel.add(spielplanGrid);
 		RootPanel.get("DetailsPanel").add(spielplanGrid);
-		
+
 	}
 }

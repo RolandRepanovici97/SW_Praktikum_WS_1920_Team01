fd27f8692048da6e0914a9963d5a5191e09c9491
Jon Gehrung <jg138@hdm-stuttgart.de>
2019-12-23 10:22:02 +0100

    GruppeMapper Test finalisiert

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/DBTester.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/server/db/DBTester.java
@@ -31,61 +31,61 @@ public abstract class DBTester {
 		 * ***************************************************************************
 		 */
 		
-		System.out.println("\nfindAllNutzer()");
-		Vector<Nutzer> nutzer = nutzerMapper.findAllNutzer();
-		Iterator i = nutzer.iterator();
-		
-		while(i.hasNext()) {
-			Nutzer user = (Nutzer) i.next();
-			System.out.println(user.toString());
-		}
-		
-		
-		System.out.println("\nfindByID(2)");
-		Nutzer testnutzerID = nutzerMapper.findByID(2);
-		System.out.println(testnutzerID.toString());
-		
-		
-		System.out.println("\nfindByEmail(Nutzer8@gmail.com)");
-		Nutzer testnutzerEmail = nutzerMapper.findByEmail("Nutzer8@gmail.com");
-		System.out.println(testnutzerEmail.toString());
-		
-		
-		System.out.println("\ninsert()");
-		Nutzer insertTest = new Nutzer();
-		insertTest.setEmail("insertTest@gmail.com");
-		insertTest.setNutzername("InsertTestNutzer");
-		System.out.println(insertTest.toString());
-		insertTest = nutzerMapper.insert(insertTest);
-		System.out.println(insertTest.toString());
-		
-		System.out.println("\nupdate()");
-		Nutzer updateTest = nutzerMapper.findByEmail("insertTest@gmail.com");
-		System.out.println(updateTest.toString());
-		updateTest.setEmail("updateTest@gmail.com");
-		updateTest.setNutzername("UpdateTest");
-		updateTest = nutzerMapper.update(updateTest);
-		System.out.println(updateTest.toString());
-		
-		System.out.println("\ndelete()");
-		Nutzer deleteTest = nutzerMapper.findByEmail("updateTest@gmail.com");
-		System.out.println(deleteTest.toString());
-		nutzerMapper.delete(deleteTest);
-		deleteTest = nutzerMapper.findByEmail("updateTest@gmail.com");
-		if(deleteTest == null) System.out.println("Der Nutzer wurde gelöscht");
-		
-		
-		System.out.println("\ndeleteGruppenzugehörigkeiten()");
-		
-		
-		System.out.println("\ngetGruppenmitgliederOf()");
-		Gruppe gruppe2 = gruppeMapper.findByID(2);
-		Vector<Nutzer> gruppenmitglieder = nutzerMapper.getGruppenmitgliederOf(gruppe2);
-		Nutzer gruppenadmin = nutzerMapper.findByID(gruppe2.getOwnerID());
-		gruppenmitglieder.add(gruppenadmin);
-		for(Nutzer n : gruppenmitglieder) {
-			System.out.println(n.toString());
-		}
+//		System.out.println("\nfindAllNutzer()");
+//		Vector<Nutzer> nutzer = nutzerMapper.findAllNutzer();
+//		Iterator i = nutzer.iterator();
+//		
+//		while(i.hasNext()) {
+//			Nutzer user = (Nutzer) i.next();
+//			System.out.println(user.toString());
+//		}
+//		
+//		
+//		System.out.println("\nfindByID(2)");
+//		Nutzer testnutzerID = nutzerMapper.findByID(2);
+//		System.out.println(testnutzerID.toString());
+//		
+//		
+//		System.out.println("\nfindByEmail(Nutzer8@gmail.com)");
+//		Nutzer testnutzerEmail = nutzerMapper.findByEmail("Nutzer8@gmail.com");
+//		System.out.println(testnutzerEmail.toString());
+//		
+//		
+//		System.out.println("\ninsert()");
+//		Nutzer insertTest = new Nutzer();
+//		insertTest.setEmail("insertTest@gmail.com");
+//		insertTest.setNutzername("InsertTestNutzer");
+//		System.out.println(insertTest.toString());
+//		insertTest = nutzerMapper.insert(insertTest);
+//		System.out.println(insertTest.toString());
+//		
+//		System.out.println("\nupdate()");
+//		Nutzer updateTest = nutzerMapper.findByEmail("insertTest@gmail.com");
+//		System.out.println(updateTest.toString());
+//		updateTest.setEmail("updateTest@gmail.com");
+//		updateTest.setNutzername("UpdateTest");
+//		updateTest = nutzerMapper.update(updateTest);
+//		System.out.println(updateTest.toString());
+//		
+//		System.out.println("\ndelete()");
+//		Nutzer deleteTest = nutzerMapper.findByEmail("updateTest@gmail.com");
+//		System.out.println(deleteTest.toString());
+//		nutzerMapper.delete(deleteTest);
+//		deleteTest = nutzerMapper.findByEmail("updateTest@gmail.com");
+//		if(deleteTest == null) System.out.println("Der Nutzer wurde gelöscht");
+//		
+//		
+//		System.out.println("\ndeleteGruppenzugehörigkeiten()");
+//		
+//		
+//		System.out.println("\ngetGruppenmitgliederOf()");
+//		Gruppe gruppe2 = gruppeMapper.findByID(2);
+//		Vector<Nutzer> gruppenmitglieder = nutzerMapper.getGruppenmitgliederOf(gruppe2);
+//		Nutzer gruppenadmin = nutzerMapper.findByID(gruppe2.getOwnerID());
+//		gruppenmitglieder.add(gruppenadmin);
+//		for(Nutzer n : gruppenmitglieder) {
+//			System.out.println(n.toString());
+//		}
 		
 		
 		/*
@@ -93,6 +93,44 @@ public abstract class DBTester {
 		 * Testen der Gruppe-Mapper Methoden
 		 * ***************************************************************************
 		 */
+		
+		System.out.println("\nfindAllGruppe()");
+		Vector <Gruppe> gruppen = gruppeMapper.findAllGruppe();
+		for(Gruppe g : gruppen) {
+			System.out.println(g.toString());
+		}
+		
+		System.out.println("\nfindById(2)");
+		Gruppe testGruppeId = gruppeMapper.findByID(2);
+		System.out.println(testGruppeId.toString());
+		
+		System.out.println("\nfindByGruppenname(Gruppe5)");
+		Gruppe testGruppeGruppenname = gruppeMapper.findByGruppenname("Gruppe5");
+		System.out.println(testGruppeGruppenname.toString());
+		
+		System.out.println("\ninsert()");
+		Gruppe gruppeInsert = new Gruppe();
+		gruppeInsert.setOwnerID(3);
+		gruppeInsert.setGruppenname("TestGruppe Insert");
+		System.out.println(gruppeInsert.toString());
+		gruppeInsert = gruppeMapper.insert(gruppeInsert);
+		System.out.println(gruppeInsert.toString());
+		
+		System.out.println("\nupdate()");
+		Gruppe gruppeUpdate = gruppeMapper.findByGruppenname("TestGruppe Insert");
+		System.out.println(gruppeUpdate.toString());
+		gruppeUpdate.setGruppenname("TestGruppeUpdate");
+		System.out.println(gruppeUpdate.toString());
+		gruppeUpdate = gruppeMapper.update(gruppeUpdate);
+		System.out.println(gruppeUpdate.toString());
+		
+		System.out.println("\ndelete()");
+		Gruppe gruppeDelete = gruppeMapper.findByGruppenname("TestGruppeUpdate");
+		System.out.println(gruppeDelete.toString());
+		gruppeMapper.delete(gruppeDelete);
+		gruppeDelete = gruppeMapper.findByGruppenname("TestGruppe Insert");
+		if(gruppeDelete == null) System.out.println("Die Gruppe wurde gelöscht");
+		
 		
 		
 		

542c70b4e60a1caea229df1a251dd24ad6d3ee4a
Alina Hafner <ah200@hdm-stuttgart.de>
2019-12-28 12:54:48 +0100

    Darstellung der Nutzerkontoform optimiert

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/editor/NutzerkontoForm.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/editor/NutzerkontoForm.java
@@ -6,6 +6,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -34,10 +35,14 @@ public class NutzerkontoForm extends HorizontalPanel {
 	private Label titel = new Label("Mein Nutzerkonto");
 	private Label nutzernamelabel = new Label("Nutzername:");
 	private TextBox nutzernametext;
-	private Label emaillabel = new Label("E-Mail");
+	private Label emaillabel = new Label("E-Mail: ");
 	private Label emailtext;
 	private Button speichernButton;
 	private VerticalPanel panelfürnutzer = new VerticalPanel();
+	private FlexTable tabelle;
+	
+	
+
 
 
 	public void onLoad() {
@@ -45,28 +50,32 @@ public class NutzerkontoForm extends HorizontalPanel {
 
 		super.onLoad();
 
+		titel.getElement().setId("TitelElemente");
+
+		tabelle = new FlexTable();
 
 		nutzernametext = new TextBox();
-//		emailtext = new Label(nutzer.getEmail());
+		//		emailtext = new Label(nutzer.getEmail());
+		emailtext = new Label("Hier steht die Mail des eingeloggten Nutzers");
 		speichernButton = new Button("Speichern");
 
-//		speichernButton.addClickHandler(new SpeichernClickHandler());
+		//		speichernButton.addClickHandler(new SpeichernClickHandler());
 
 		panelfürnutzer.add(titel);
-		panelfürnutzer.add(nutzernamelabel);
-		panelfürnutzer.add(nutzernametext);
-		panelfürnutzer.add(emaillabel);
-//		panelfürnutzer.add(emailtext);
+		tabelle.setWidget(1, 1, nutzernamelabel);
+		tabelle.setWidget(1, 2, nutzernametext);;
+		tabelle.setWidget(2,1, emaillabel);
+		tabelle.setWidget(2, 2, emailtext);
+		panelfürnutzer.add(tabelle);
+		//		panelfürnutzer.add(emailtext);
 		panelfürnutzer.add(speichernButton);
-		
+
 		this.add(panelfürnutzer);
-		this.add(speichernButton);
-		this.add(nutzernametext);
-		
-	//	RootPanel.get().add(this);
+
+		//	RootPanel.get().add(this);
 
 	}
-	
+
 
 
 	/*
@@ -83,47 +92,47 @@ public class NutzerkontoForm extends HorizontalPanel {
 	 * @author alina
 	 */
 
-//	class SpeichernClickHandler implements ClickHandler{
-//
-//
-//		@Override
-//		public void onClick(ClickEvent event) {
-//			//Wenn die Textbox zum Nutzernamen befüllt ist, dann...
-//
-//			if(nutzernametext!= null) {
-//				//Update des Nutzerobjekts. Implizit wird das Nutzerobjekt in der Datenbank aktualisiert.
-//				kinobesuchsplanung.save(nutzer, new NutzerCallback());
-//
-//			}
-//
-//			//Andernfalls hat der Nutzers nichts geändert...
-//			else {
-//				Window.alert("Sie haben keine Änderungen ausgeführt");
-//			}
-//
-//		}
-//
-//	}
-//
-//	/**
-//	 * Diese Nested Class implementiert das Interface AsyncCallBack und ermöglicht die Rückgabe des Nutzerobjekts.
-//	 * @author alina
-//	 */
-//	class NutzerCallback implements AsyncCallback <Void>{
-//
-//		@Override
-//		public void onFailure(Throwable caught) {
-//			Window.alert("Ihr Nutzerkonto konnte nicht aktualisiert werden");
-//
-//		}
-//
-//
-//		@Override
-//		public void onSuccess(Void result) {
-//			// TODO Auto-generated method stub
-//
-//		}
-//
-//	}
-//
+	//	class SpeichernClickHandler implements ClickHandler{
+	//
+	//
+	//		@Override
+	//		public void onClick(ClickEvent event) {
+	//			//Wenn die Textbox zum Nutzernamen befüllt ist, dann...
+	//
+	//			if(nutzernametext!= null) {
+	//				//Update des Nutzerobjekts. Implizit wird das Nutzerobjekt in der Datenbank aktualisiert.
+	//				kinobesuchsplanung.save(nutzer, new NutzerCallback());
+	//
+	//			}
+	//
+	//			//Andernfalls hat der Nutzers nichts geändert...
+	//			else {
+	//				Window.alert("Sie haben keine Änderungen ausgeführt");
+	//			}
+	//
+	//		}
+	//
+	//	}
+	//
+	//	/**
+	//	 * Diese Nested Class implementiert das Interface AsyncCallBack und ermöglicht die Rückgabe des Nutzerobjekts.
+	//	 * @author alina
+	//	 */
+	//	class NutzerCallback implements AsyncCallback <Void>{
+	//
+	//		@Override
+	//		public void onFailure(Throwable caught) {
+	//			Window.alert("Ihr Nutzerkonto konnte nicht aktualisiert werden");
+	//
+	//		}
+	//
+	//
+	//		@Override
+	//		public void onSuccess(Void result) {
+	//			// TODO Auto-generated method stub
+	//
+	//		}
+	//
+	//	}
+	//
 }

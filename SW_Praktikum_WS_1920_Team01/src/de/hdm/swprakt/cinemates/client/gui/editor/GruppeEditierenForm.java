575d3e00cff28e062418fe4ad7fb210631cbecc3
Roland Repanovici <56630714+RolandRepanovici97@users.noreply.github.com>
2020-01-07 21:49:21 +0100

    weiter bearbeitet. morgen fertig.

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/editor/GruppeEditierenForm.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/editor/GruppeEditierenForm.java
@@ -1,5 +1,9 @@
 package de.hdm.swprakt.cinemates.client.gui.editor;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -19,48 +23,182 @@ import de.hdm.swprakt.cinemates.shared.bo.Gruppe;
 
 
 public class GruppeEditierenForm extends HorizontalPanel {
-	
+
 	//Benötigte Klassenvariable, stellt die selektierte Gruppe dar
 	private Gruppe gewählteGruppe;
-	
+
 	//Setzen der asynchronen Interfaces
 	KinoBesuchsplanungAsync kinobesuchsplanung = ClientSideSettings.getKinobesuchsplanung();
 	KinoAdministrationAsync kinoadministration = ClientSideSettings.getKinoAdministration();
-	
+
 	//Initialisierung der benötigten Widgets
 	private Label titel;
 	private Grid tabelle;
-	private Label gruppename = new Label("Neuer Gruppename: ");
-	private TextBox gruppennameText = new TextBox();
-	private Button speichernButton = new Button("Speichern");
+	private Label gruppename = new Label("Gruppename: ");
+	private TextBox gruppenameText = new TextBox();
+	private Button speichernButton = new Button("Gruppe speichern");
 	private Button löschenButton = new Button();
 	private Label mitgliedern = new Label("Aktuelle Mitgliedern: ");
 	private Button mitgliedlöschen = new Button("Mitglied löschen");
 	private Button neuesmitglied = new Button ("Neues Mitglied hinfügen");
-	private TextBox neuesmitgliedeingeben = new TextBox();
-	
+	private TextBox neuesmitgliedText = new TextBox();
+	private HorizontalPanel horizontalPanel;
+
+
 	// Getter & Setter für die Variable Gruppe: Wird benötigt, wenn eine Gruppe selektiert wurde
-	
+
 	/**
 	 * @return the gewählteGruppe
 	 */
 	public Gruppe getGewählteGruppe() {
 		return gewählteGruppe;
 	}
-	
+
 	/**
 	 * @param gewählteGruppe the gewählteGruppe to set
 	 */
 	public void setGewählteGruppe(Gruppe gewählteGruppe) {
 		this.gewählteGruppe = gewählteGruppe;
 	}
-	
+
 	public void onLoad() {
 		super.onLoad();
-		
+
 		löschenButton.setHTML("<i class=\"far fa-trash-alt\"> <br> Gruppe löschen</i>");
-		
+
 		//Panel um zwei Button nebeneinander anzuordnen
+
+		horizontalPanel = new HorizontalPanel();
+
+		titel = new Label("Gruppe: " + gewählteGruppe.getGruppenname() + "bearbeiten");
+		titel.getElement().setId("TitelElemente");
+		gruppenameText.setText(gewählteGruppe.getGruppenname());
+
+		//kinobesuchsplanung.getAllNutzerOfGruppe(gewählteGruppe, new MitgliederAnzeigenCallback());
+
+
+		//Befüllen der Tabelle
+
+		tabelle = new Grid(6,3);
+
+		tabelle.setWidget(1, 1, gruppename);
+		tabelle.setWidget(1, 2, gruppenameText);
+		tabelle.setWidget(2, 1, mitgliedern);
+
+
+	}
+
+
+	/*
+	 * ***************************************************************************
+	 * ABSCHNITT: Hier wird der zweite Teil der GruppenVerwaltungSeite implementiert.
+	 * Hier werde die aktuelle gewählte Gruppe des angemeldeten Nutzers angezeigt. 
+	 * Er hat hier die Möglichkeit, den Gruppenname zu editieren, aktuelle Mitglieder
+	 * zu löschen und neue hinzüfügen. Der Nutzer kann auch die Gruppe löschen oder
+	 * alle seine Änderungen speichern.
+	 * ***************************************************************************
+	 */
+
+	/*
+	 * ***************************************************************************
+	 * ABSCHNITT Nested Classes
+	 * ***************************************************************************
+	 */
+
+	
+	/**
+	 * Diese Nested Class implementiert das Interface AsyncCallback und ermöglicht
+	 * die Rückgabe des editierten Gruppeobjekts
+	 * 
+	 * @author roland
+	 */
+	class GruppeCallback implements AsyncCallback<Gruppe> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Ihre Gruppe konnte nicht geladen werden");
+		}
+
+		@Override
+		public void onSuccess(Gruppe result) {
+			Window.alert("Die Gruppe wurde erfolgreich editiert!");
+		}
+	}
+
+	
+	/**
+	 * Diese Nested Class implementiert das Interface AsyncCallback und das Löschen
+	 * einer selktierten Gruppe
+	 * 
+	 * @author roland
+	 */
+	class LöschenCallback implements AsyncCallback<Void> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Ihre Gruppe konnte nicht gelöscht werden");
+		}
+
+		@Override
+		public void onSuccess(Void result) {
+			Window.alert("Die Gruppe wurde erfolgreich gelöscht!");
+		}
+	}
+
+	
+	/**
+	 * Diese Nested Class implementiert das Interface ClickHandler.
+	 * Klickt der Nutzer diesen Button an, so werden die Änderungen an der Gruppe gespeichert
+	 * 
+	 * @author Roland
+	 */
+
+	class SpeichernClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+			gewählteGruppe.setGruppenname(gruppenameText.getText());
+			//kinobesuchsplanung.save(gewählteGruppe, new GruppeCallback());
+			
+		}
+			
 	}
 	
+	/**
+	 * Diese Nested Class implmentiert das Interface ClickHandler.
+	 * Klickt der Nutzer diessen Button an, so wird die Gruppe gelöscht.
+	 * 
+	 * @author Roland
+	 */
+
+	class LöschenClickHandler implements ClickHandler {
+		
+		@Override
+		public void onClick(ClickEvent event) {
+			kinobesuchsplanung.deleteGruppe(gewählteGruppe, new LöschenCallback());
+		}
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

d954073e27d5f3c1679f6c842dee4aeac6d3227f
ÖmerDegirmenci <oemer.degirmenci@hotmail.de>
2020-01-08 09:39:05 +0100

    Alle Filme anzeigen

--- SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/admin/FilmForm.java
+++ SW_Praktikum_WS_1920_Team01/src/de/hdm/swprakt/cinemates/client/gui/admin/FilmForm.java
@@ -6,14 +6,13 @@ import java.util.Vector;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -28,45 +27,44 @@ import de.hdm.swprakt.cinemates.shared.KinoAdministrationAsync;
 import de.hdm.swprakt.cinemates.shared.bo.Film;
 import de.hdm.swprakt.cinemates.shared.bo.Kino;
 
-
 /**
- * Die Klasse erweitert das HorizontalPanel.Die Klasse wird benötigt um neue Film zu erstellen und diese
- * dann weiter unten als Tabelle auszugeben. Hier werden alle Filme aus Datenbank angezeigt.
+ * Die Klasse erweitert das HorizontalPanel.Die Klasse wird benötigt um neue
+ * Film zu erstellen und diese dann weiter unten als Tabelle auszugeben. Hier
+ * werden alle Filme aus Datenbank angezeigt.
+ * 
  * @author oemer
  *
  */
 
 public class FilmForm extends HorizontalPanel {
 
-	//Erzeugen der Widgets
+	// Erzeugen der Widgets
 	TextBox filmname = new TextBox();
 	TextBox filmbeschreibung = new TextBox();
 	TextBox spiellange = new TextBox();
 	Button filmanlegen = new Button("Film anlegen");
 	private Button ja = new Button("JA");
 	private Button nein = new Button("NEIN");
+	private FlexTable flexTable;
 
 	VerticalPanel detailsPanel = new VerticalPanel();
 
 	// Ein ListDataProvider wird erstellt um die Daten auszugeebn
-//	private ListDataProvider<Film> dataProvider = new ListDataProvider<Film>();
+	private ListDataProvider<Film> dataProvider = new ListDataProvider<Film>();
 
-	
 	// Setzen der asynchronen Interfaces
 	KinoAdministrationAsync kinoAdministration = ClientSideSettings.getKinoAdministration();
 
-// bisher leerer Konstruktor 
+	// bisher leerer Konstruktor 
 	public FilmForm() {
 
 	}
 
 	// Die Liste der Daten anzeigen
-	private static List<Film> FILMS = Arrays.asList();
+	// private static List<Film> FILMS = Arrays.asList();
 
-	
-	
 	/**
-	 * onload()-Methode 
+	 * onload()-Methode
 	 */
 	public void onLoad() {
 
@@ -93,60 +91,62 @@ public class FilmForm extends HorizontalPanel {
 		RootPanel.get("DetailsPanel").add(detailsPanel);
 		this.add(detailsPanel);
 
-		  FlexTable flexTable = new FlexTable();
-		
-		
-	flexTable.setText(0, 1, "Film");
-	flexTable.setText(0, 2, "Beschreibung");
+		flexTable = new FlexTable();
+
+		flexTable.setText(0, 1, "Film");
+		flexTable.setText(0, 2, "Beschreibung");
 		flexTable.setText(0, 3, "Spiellänge");
-		
+
+		filmanlegen.addClickHandler(new FilmAnlegenClickHandler());
+		// dataProvider.addDataDisplay(flexTable);
 		detailsPanel.add(flexTable);
 
+		//		Button addRowButton = new Button("Add a Row"); 
+		//      addRowButton.addClickHandler(new ClickHandler() {
+		//	         @Override
+		//         public void onClick(ClickEvent event) {
+		//	            addRow(flexTable);
+		//         }
+		//	      });
+	}
 
-		class FilmAnlegenCallback implements AsyncCallback<Film> {
-			@Override
-			public void onFailure(Throwable caught) {
-				Window.alert("Der Film konnte nicht erstellt werden");
-			}
+	class FilmAnlegenClickHandler implements ClickHandler {
+		public void onClick(ClickEvent event) {
 
-			@Override
-			public void onSuccess(Film result) {
-
-				Window.alert("Der Film wurde erstellt");
-
-				filmname.setText("");
-				filmbeschreibung.setText("");
-				spiellange.setText("");
-
-			
-				
-				flexTable.setText(1, 1, result.getFilmtitel());
-				flexTable.setText(1, 2, result.getBeschreibung());
-				flexTable.setText(1, 3, result.getDetails());
-
-			}
+			kinoAdministration.createFilm(filmname.getText(), filmbeschreibung.getText(), spiellange.getText(),
+					new FilmAnlegenCallback());
+			// addRow(flexTable);
 
 		}
+	}
 
-//		Button addRowButton = new Button("Add a Row"); 
-//      addRowButton.addClickHandler(new ClickHandler() {
-//	         @Override
-//         public void onClick(ClickEvent event) {
-//	            addRow(flexTable);
-//         }
-//	      });
-
-		class FilmAnlegenClickHandler implements ClickHandler {
-			public void onClick(ClickEvent event) {
-
-				kinoAdministration.createFilm(filmname.getText(), filmbeschreibung.getText(), spiellange.getText(),
-						new FilmAnlegenCallback());
-				// addRow(flexTable);
-			}
+	class FilmAnlegenCallback implements AsyncCallback<Film> {
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Der Film konnte nicht erstellt werden");
 		}
-		filmanlegen.addClickHandler(new FilmAnlegenClickHandler());
+
+		@Override
+		public void onSuccess(Film result) {
+
+
+			Window.alert("Der Film wurde erstellt");
+
+			kinoAdministration.getAllFilme(new AlleFilmeCallback());
+			//
+			//			filmname.setText("");
+			//			filmbeschreibung.setText("");
+			//			spiellange.setText("");
+			//
+			//			flexTable.setText(1, 1, result.getFilmtitel());
+			//			flexTable.setText(1, 2, result.getBeschreibung());
+			//			flexTable.setText(1, 3, result.getDetails());
+
+
+		}
 
 	}
+
 
 	class AlleFilmeCallback implements AsyncCallback<Vector<Film>> {
 		@Override
@@ -158,61 +158,66 @@ public class FilmForm extends HorizontalPanel {
 		public void onSuccess(Vector<Film> result) {
 			// TODO Auto-generated method stub
 			Window.alert("Die Filme.... ");
+			int rowCount = 1;
+			for (Film f: result) {
 
-			filmname.setText("");
-			filmbeschreibung.setText("");
-			spiellange.setText("");
+				flexTable.setText(rowCount, 1, f.getFilmtitel());
+				flexTable.setText(rowCount, 2, f.getBeschreibung());
+				flexTable.setText(rowCount, 3, f.getDetails());
+				rowCount++;
 
-//			flexTable.setText(1, 1, result.getFilmtitel());
-//			flexTable.setText(1, 2, result.getBeschreibung());
-//			flexTable.setText(1, 3, result.getDetails());
 
-		}
-
-	}
-
-	class AlleFilmeClickHandler implements ClickHandler {
-		public void onClick(ClickEvent event) {
-
-			kinoAdministration.getAllFilme(new AlleFilmeCallback());
-
-		}
-	}
-	
-	
-	
-	class FilmLöschenClickHandler extends DialogBox implements ClickHandler {
-
-		public FilmLöschenClickHandler() {
-			setText("Möchten Sie das Kino wirklich löschen?");
-			Grid jaNein = new Grid(3, 3);
-			jaNein.setWidget(0, 1, ja);
-			jaNein.setWidget(0, 2, nein);
-			nein.addClickHandler(new neinClickHandler());
-			setAnimationEnabled(false);
-			setGlassEnabled(false);
-			this.add(jaNein);
-		}
-
-		public void onClick(ClickEvent event) {
-			new FilmLöschenClickHandler().show();
-		}
-
-		private class neinClickHandler implements ClickHandler {
-			@Override
-			public void onClick(ClickEvent event) {
-				RootPanel.get().clear();
 			}
 		}
+
 	}
-
-//	
-//		/**
-//		    * Add a row to the flex table.
-//		    */
-//		   private void addRow(FlexTable flexTable) {
-//		  
-//			   int numRows = flexTable.getRowCount();
-//		     
-
 }
+//class AlleFilmeClickHandler implements ClickHandler {
+//	public void onClick(ClickEvent event) {
+//
+//		kinoAdministration.getAllFilme(new AlleFilmeCallback());
+//
+//	}
+//}
+
+//class FilmLöschenClickHandler extends DialogBox implements ClickHandler {
+//
+//	public FilmLöschenClickHandler() {
+//		setText("Möchten Sie das Kino wirklich löschen?");
+//		Grid jaNein = new Grid(3, 3);
+//		jaNein.setWidget(0, 1, ja);
+//		jaNein.setWidget(0, 2, nein);
+//		nein.addClickHandler(new neinClickHandler());
+//		setAnimationEnabled(false);
+//		setGlassEnabled(false);
+//		this.add(jaNein);
+//	}
+//
+//	public void onClick(ClickEvent event) {
+//		new FilmLöschenClickHandler().show();
+//	}
+//
+//	private class neinClickHandler implements ClickHandler {
+//		@Override
+//		public void onClick(ClickEvent event) {
+//			RootPanel.get().clear();
+//		}
+//	}
+//}
+//
+////	
+////		/**
+////		  * Add a row to the flex table.
+////		 */
+////	private void addRow(FlexTable flexTable) {
+//
+//int numRows = flexTable.getRowCount();
+//flexTable.setWidget(numRows, 1, new HTML(filmname.getText()));
+//flexTable.setWidget(numRows, 2, new HTML(filmbeschreibung.getText()));
+//flexTable.setWidget(numRows, 3, new HTML(spiellange.getText()));
+////		flexTable.getFlexCellFormatter().setRowSpan(0, 1, numRows +1);
+//
+//
+////		     
+//
+//}}
